5b395175883c391a19523ffe93e644fa0df7efea
andipartman-collab <andipartman@gmail.com>
2026-09-14 23:04:23 +0700

    Respect scanner sound setting when playing scan sounds

--- lib/core/services/scanner_sound_service.dart
+++ lib/core/services/scanner_sound_service.dart
@@ -1,9 +1,20 @@
 import 'package:audioplayers/audioplayers.dart';
 
+import 'local_storage_service.dart';
+
 class ScannerSoundService {
   final AudioPlayer _player = AudioPlayer();
+  final LocalStorageService _storage =
+      LocalStorageService();
 
   Future<void> playSuccess() async {
+    final enabled =
+        await _storage.getScannerSoundEnabled();
+
+    if (!enabled) {
+      return;
+    }
+
     await _player.stop();
 
     await _player.play(
@@ -12,6 +23,13 @@ class ScannerSoundService {
   }
 
   Future<void> playError() async {
+    final enabled =
+        await _storage.getScannerSoundEnabled();
+
+    if (!enabled) {
+      return;
+    }
+
     await _player.stop();
 
     await _player.play(
